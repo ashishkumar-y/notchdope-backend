0408e68cc20dfbdcd2f33dd190c19a8644992331
Ashish Kumar <ashishkumar@Sumits-MacBook-Air.local>
2025-07-05 19:57:42 +0530

    commit

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install missing libraries for Puppeteer & Chrome
+apt-get update && apt-get install -y \
+  wget \
+  ca-certificates \
+  fonts-liberation \
+  libappindicator3-1 \
+  libasound2 \
+  libatk-bridge2.0-0 \
+  libatk1.0-0 \
+  libcups2 \
+  libdbus-1-3 \
+  libgdk-pixbuf2.0-0 \
+  libnspr4 \
+  libnss3 \
+  libx11-xcb1 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  xdg-utils \
+  --no-install-recommends
+
+# Start the Node.js server
+node index.js
